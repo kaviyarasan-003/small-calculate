5806293da8297994e7d235897d91d05965220d68
kaviyarasan-003 <kaviyarasan142001@gmail.com>
2024-08-02 12:51:05 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,30 @@
+
+function adding(){
+ const a=parseInt(document.getElementById('num1').value)
+ const b=parseInt(document.getElementById('num2').value)
+ alert(a+b)
+ alert("Thanks for adding")
+
+}
+
+
+function subtraction(){
+    const a=document.getElementById('num1').value
+    const b=document.getElementById('num2').value
+    alert(a-b)
+   
+   }
+   
+function multiplication(){
+    const a=document.getElementById('num1').value
+    const b=document.getElementById('num2').value
+    alert(a*b)
+   
+   }
+   
+function division(){
+    const a=document.getElementById('num1').value
+    const b=document.getElementById('num2').value
+    alert(a/b)
+   
+   }
